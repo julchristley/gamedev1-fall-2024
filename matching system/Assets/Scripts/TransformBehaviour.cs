086c4e9c819f534920b52067029f31b7203f8277
Juliette <julchristley@gmail.com>
2024-09-09 22:23:30 -0600

    matching game lab done

---
+++ matching system/Assets/Scripts/TransformBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformBehaviour : MonoBehaviour
+{
+    public void ResetToZero()
+    {
+        // resets object's location, I think
+        transform.position = Vector3.zero;
+    }
+}
